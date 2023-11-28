5ba5e02736581929a6878545e54b9e5c7f9575a2
Saymon Araújo <67705166+saymon-araujo@users.noreply.github.com>
2023-11-28 07:39:17 -0300

    chore: create styles for react components

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from "./styles";
+
+const Button = styled("button", {
+  display: "flex",
+});
+
+export function App() {
+  return <h1></h1>;
+}
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,29 @@
+import { createStitches, defaultThemeMap } from "@stitches/react";
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from "@kidohka-ui/tokens";
+
+export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } =
+  createStitches({
+    themeMap: {
+      ...defaultThemeMap,
+      height: "space",
+      width: "space",
+    },
+
+    theme: {
+      colors,
+      fonts,
+      fontSizes,
+      lineHeights,
+      fontWeights,
+      radii,
+      space,
+    },
+  });
