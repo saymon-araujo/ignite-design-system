a0533c0a92df7e82c8213be4f361938177ccebe9
Saymon Araújo <67705166+saymon-araujo@users.noreply.github.com>
2023-11-29 07:55:16 -0300

    chore: add docs to tokens

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,22 @@
+import { colors } from "@kidohka-ui/tokens";
+import { getContrast } from "polished";
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, value]) => {
+    return (
+      <div key={key} style={{ backgroundColor: value, padding: "2rem" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            fontFamily: "monospace",
+            color: getContrast(value, "#fff") < 3.5 ? "#000" : "#fff",
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{value}</span>
+        </div>
+      </div>
+    );
+  });
+}
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import { StoryObj, Meta } from "@storybook/react";
 import { Button, ButtonProps } from "@kidohka-ui/react";
 
 export default {
-  title: "Button",
+  title: "Form/Button",
   component: Button,
 
   args: {
